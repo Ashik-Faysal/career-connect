81e76f2b64beb5194c5a4d36a3b6173fb41cb9cc
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-04-09 21:42:00 +0600

    included daisy UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1ac951",
 
+          secondary: "#59d6a8",
+
+          accent: "#e2bd36",
+
+          neutral: "#1A141F",
+
+          "base-100": "#383838",
+
+          info: "#5080E7",
+
+          success: "#5DE9CD",
+
+          warning: "#EFA861",
+
+          error: "#FA3368",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};

991f7b2cb7ca0c141c3dd0ed521ffe7bb3926bae
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-04-12 14:38:01 +0600

    change headeer menu color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="md:flex justify-between items-center my_container bg-gray-200 p-6 text-black rounded-lg">
-      <h2 className="text-5xl font-bold mb-4">Career Connect</h2>
+    <nav className="md:flex justify-between items-center my_container bg-gray-200 p-6 rounded-lg">
+      <h2 className="text-5xl font-bold mb-4">
+        <span className="text-lime-700">Career</span> Connect
+      </h2>
       <div className="flex flex-col md:flex-row gap-4 font-semibold">
-        <Link className="btn-link hover:text-blue-300" to="/">
+        <Link className="btn-link text-stone-600 hover:text-stone-950 text-2xl font-semibold" to="/">
           Home
         </Link>
-        <Link className="btn-link hover:text-blue-300" to="/statistics">
+        <Link className="btn-link text-stone-600 hover:text-stone-950 text-2xl font-semibold" to="/statistics">
           Statistics
         </Link>
-        <Link className="btn-link hover:text-blue-300" to="/applied">
+        <Link className="btn-link text-stone-600 hover:text-stone-950 text-2xl font-semibold" to="/applied">
           Applied Job
         </Link>
-        <Link className="btn-link hover:text-blue-300" to="/blog">
+        <Link className="btn-link text-stone-600 hover:text-stone-950 text-2xl font-semibold" to="/blog">
           Blog
         </Link>
       </div>
-      <button className="hidden md:block btn btn-info">Start Applying</button>
+      <button className="mt-4 btn btn-info">Start Applying</button>
     </nav>
   );
 };

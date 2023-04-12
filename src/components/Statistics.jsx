d9dfb725f1104bb6c60774478fa5c68f4b074aa6
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-04-12 21:06:03 +0600

    added a text on statistic file

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -17,24 +17,27 @@ const Statistics = () => {
   }, []);
   // console.log(chart.marks);
   return (
-    <RadarChart
-      className="mx-auto mb-56"
-      width={600}
-      height={400}
-      data={chart.marks}
-    >
-      <PolarGrid />
-      <PolarAngleAxis dataKey="name" />
-      <PolarRadiusAxis />
-      <Radar
-        name="Score"
-        dataKey="score"
-        stroke="#8884d8"
-        fill="#8884d8"
-        fillOpacity={0.6}
-      />
-      <Tooltip />
-    </RadarChart>
+    <>
+      <h1 className="text-5xl font-bold mx-4 md:mx-24 text-violet-800 text-center mb-4">Here is my 8 Assignment Marks</h1>
+      <RadarChart
+        className="mx-auto mb-56"
+        width={600}
+        height={400}
+        data={chart.marks}
+      >
+        <PolarGrid />
+        <PolarAngleAxis dataKey="name" />
+        <PolarRadiusAxis />
+        <Radar
+          name="Score"
+          dataKey="score"
+          stroke="#8884d8"
+          fill="#8884d8"
+          fillOpacity={0.6}
+        />
+        <Tooltip />
+      </RadarChart>
+    </>
   );
 };
 
